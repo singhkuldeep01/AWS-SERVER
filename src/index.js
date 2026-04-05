032eb68b2889f777cae018786dfafad37821e856
iamknullgodofvoid-source <iamknullgodofvoid@gmail.com>
2026-04-05 14:45:17 +0530

    code added

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import ip from 'ip';
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/ping' , (req, res)=>{
+    res.json({
+        ip: ip.address(),
+        message: "pong",
+        timestamp: new Date().toISOString()
+    });   
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
