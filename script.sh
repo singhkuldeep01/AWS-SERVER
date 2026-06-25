1bae9a55353f6a7f609d8a7dc25659d6e5865feb
Kuldeep singh <158292877+singhkuldeep01@users.noreply.github.com>
2026-06-25 21:25:45 +0530

    Refactor script.sh for package installation and comments
    Updated script to remove sudo for package installations and added comments for clarity.

--- script.sh
+++ script.sh
@@ -2,17 +2,19 @@
 
 exec > /var/log/user-data.log 2>&1
 
-# Update system
-sudo apt update -y
+# Update packages
+apt update -y
 
-# Install Node.js (better version)
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# Install Git, Curl and Node.js
+apt install -y git curl
+
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+apt install -y nodejs
 
 # Install PM2
-sudo npm install -g pm2
+npm install -g pm2
 
-# Clone correct repo
+# Clone repository
 git clone https://github.com/singhkuldeep01/AWS-SERVER.git /home/ubuntu/project
 
 # Go to project
@@ -21,8 +23,11 @@ cd /home/ubuntu/project
 # Install dependencies
 npm install
 
-# Start server (correct path)
-sudo pm2 start src/index.js --name node-server
+# Start application
+pm2 start src/index.js --name node-server
 
 # Save PM2 process
 pm2 save
+
+# Configure PM2 to start on reboot
+env PATH=$PATH:/usr/bin pm2 startup systemd -u ubuntu --hp /home/ubuntu
